d89f5135aa27f31a47dec2c7865fd4492f4e3456
Markusx <markusbk@hotmail.com>
2017-09-11 21:04:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var main = function (params) {
 
     var request = require('request');
-    var url ='https://requestb.in/riq0kfri'
+    var url ='https://requestb.in/riq0kfri?test=hi'
     request(url, function (error, response, body) {
       if (!error) {
         console.log(body);

a1a14adaf93ae424d4b6d094ebfe846bf19f240d
Markusx <markusbk@hotmail.com>
2017-09-11 21:55:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 var main = function (params) {
 
     var request = require("request");
-
+var options = {
+  url: 'https://api.github.com/repos/request/request',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+    request.post(
+        {
+            uri:'https://api.line.me/v2/bot/message/push',
+            
+            headers : {
+                'Authorization' : 'Bearer: ' +params.lineKey,
+                'Content-Type':'Content-Type:application/json'
+            },
+            
+            json: {
+                to:params.userId,
+                messages:[ {
+                 type:'text',
+                 text:'we.Ride!!'   
+                }]
+            }
+        }
+    );
  
 
     return {hello:'world',details:params.events};

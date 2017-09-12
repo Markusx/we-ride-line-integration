69a12d7d8e67b15ffe81567787085a870a829beb
Markusx <markusbk@hotmail.com>
2017-09-11 21:00:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var main = function (params) {
 
+    var request = require('request');
+    var url ='https://requestb.in/riq0kfri'
+    request(url, function (error, response, body) {
+      if (!error) {
+        console.log(body);
+      }
+    });
+
     return {hello:'world'};
 
 };

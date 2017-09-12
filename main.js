33bfb20bd82eb021383e942b55352d2dcfca5f72
Markusx <markusbk@hotmail.com>
2017-09-11 21:25:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,14 @@ var main = function (params) {
 
     var request = require("request");
 
-    var jar = request.jar();
-    jar.setCookie(request.cookie("foo=bar"), "http://mockbin.org/bin/87a6111d-7399-40c4-ad96-987787bd0537");
-    jar.setCookie(request.cookie("bar=baz"), "http://mockbin.org/bin/87a6111d-7399-40c4-ad96-987787bd0537");
-
+ 
     var options = { method: 'POST',
       url: 'http://mockbin.org/bin/87a6111d-7399-40c4-ad96-987787bd0537',
       qs: { foo: params },
       headers: 
        { 'content-type': 'application/x-www-form-urlencoded',
          accept: 'application/json' },
-      form: { foo: 'bar', bar: 'baz' },
+      form: { foo: params },
       jar: 'JAR' };
 
     request(options, function (error, response, body) {

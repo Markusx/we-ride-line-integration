561666c0266b5b09436b0e137e8656b4592f56db
Markusx <markusbk@hotmail.com>
2017-09-11 21:25:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 var main = function (params) {
 
-    var request = require('request');
-    var url ='https://requestb.in/13j68c81?test=yes'
-    request(url, function (error, response, body) {
-      if (!error) {
-        console.log(body);
-      }
+    var request = require("request");
+
+    var jar = request.jar();
+    jar.setCookie(request.cookie("foo=bar"), "http://mockbin.org/bin/87a6111d-7399-40c4-ad96-987787bd0537");
+    jar.setCookie(request.cookie("bar=baz"), "http://mockbin.org/bin/87a6111d-7399-40c4-ad96-987787bd0537");
+
+    var options = { method: 'POST',
+      url: 'http://mockbin.org/bin/87a6111d-7399-40c4-ad96-987787bd0537',
+      qs: { foo: params },
+      headers: 
+       { 'content-type': 'application/x-www-form-urlencoded',
+         accept: 'application/json' },
+      form: { foo: 'bar', bar: 'baz' },
+      jar: 'JAR' };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+
+      console.log(body);
     });
 
     return {hello:'world'};
